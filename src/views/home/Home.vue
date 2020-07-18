035d1862bcd5cccdb105c4550e1cd2a16d74027b
cod-star <1335564743@qq.com>
2020-07-18 22:04:49 +0800

    master

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <nav-bar class="home-nav"><div slot="center">购物街</div> </nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <home-feature/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
 import HomeSwiper from './childComps/HomeSwiper';
+import RecommendView from './childComps/RecommendView';
+import HomeFeature from './childComps/HomeFeature'
 
 
 import {getHomeMultidata} from 'network/home.js';
@@ -21,7 +23,9 @@ export default {
   name:"Home",
   components: {
     NavBar,
-    HomeSwiper 
+    HomeSwiper,
+    RecommendView,
+    HomeFeature
   },
   data() {
     return {
@@ -35,6 +39,7 @@ export default {
       // this.reslut = res;
       // console.log(res);
       this.banners = res.data.banner.list
+      // console.log(this.banners);
       this.recommends = res.data.recommend.list
     })
   },
@@ -44,9 +49,19 @@ export default {
 
 <style>
 
+#home{
+  padding-top: 44px;
+}
+
 .home-nav{
   background-color: var(--color-tint);
   color: white;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
   
 </style>
---
+++ src/views/home/childComps/HomeFeature.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+    <img src="~assets/img/home/feature.jpg" alt="">
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeFeature'
+}
+</script>
+<style>
+  .feature img{
+    width: 100%;
+    height: 200px;
+  }
+</style>
