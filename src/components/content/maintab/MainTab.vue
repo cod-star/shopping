9994c8f4cc23fee376aac15cc9b9ae4935b052c5
cod-star <1335564743@qq.com>
2020-07-18 13:57:26 +0800

    gitHub

--- src/components/content/maintab/MainTab.vue
+++ src/components/content/maintab/MainTab.vue
@@ -1,21 +1,21 @@
 <template>
    <tab-bar>
-      <tab-bar-item path='/home' activeColor="pink">
+      <tab-bar-item path='/home' activeColor="red">
         <img slot="item-icon" src="~assets/img/tabbar/home.png"  alt="" />
         <img slot="item-icon-active" src="~assets/img/tabbar/homeactive.png"  alt="" />
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item path='/category' activeColor="pink">
+      <tab-bar-item path='/category' activeColor="red">
          <img slot="item-icon" src="~assets/img/tabbar/wenj.png"  alt="" />
           <img slot="item-icon-active" src="~assets/img/tabbar/wenjactive.png"  alt="" />
         <div slot="item-text">分类</div>
       </tab-bar-item >
-      <tab-bar-item path='/cart' activeColor="pink">
+      <tab-bar-item path='/cart' activeColor="red">
          <img slot="item-icon" src="~assets/img/tabbar/gouwu.png"  alt="" />
           <img slot="item-icon-active" src="~assets/img/tabbar/gouwuactive.png"  alt="" />
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item path='/profile' activeColor="pink">
+      <tab-bar-item path='/profile' activeColor="red">
         <img slot="item-icon" src="~assets/img/tabbar/ren.png"  alt="" />
          <img slot="item-icon-active" src="~assets/img/tabbar/renactive.png"  alt="" />
         <div slot="item-text">我的</div>
